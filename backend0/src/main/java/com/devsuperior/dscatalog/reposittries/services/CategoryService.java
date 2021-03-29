03ff43cc9a514bdd29a5a93aeaefb719f5e3b448
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-03-28 21:29:57 -0300

    Uptade Category

--- backend0/src/main/java/com/devsuperior/dscatalog/reposittries/services/CategoryService.java
+++ backend0/src/main/java/com/devsuperior/dscatalog/reposittries/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -37,6 +39,19 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+		
+	}
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found " + e.getMessage());
+		}
 	}
 
 }
